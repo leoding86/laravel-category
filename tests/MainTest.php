8675a5a842607bfdd6be796abdc260af49cf142a
leoding86 <leoding86@msn.com>
2018-10-30 17:46:50 +0800

    update test

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -2,7 +2,12 @@
 
 namespace LDing\Tests\LaravelCategory;
 
-use Tests\TestCase;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Container\Container as Application;
+use Illuminate\Support\Facades\Facade;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Illuminate\Cache\CacheManager;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\DB;
 use LDing\LaravelCategory\Exceptions\DifferentRelatedModelException;
 use LDing\LaravelCategory\Exceptions\RemoveCategoryHasChildException;
@@ -11,6 +16,53 @@ use LDing\LaravelCategory\Models\Category;
 
 class MainTest extends TestCase
 {
+    protected static $app;
+
+    public function setUp()
+    {
+        static::$app = new Application;
+        Application::setInstance(static::$app);
+        Facade::setFacadeApplication(static::$app);
+
+        static::$app->singleton('config', function () {
+            return [
+                'path.storage' => __DIR__ . '/storage/cache',
+                'cache.default' => 'file',
+                'cache.stores.file' => [
+                    'driver' => 'file',
+                    'path' => __DIR__ . '/storage/cache',
+                ]
+            ];
+        });
+
+        static::$app->singleton('db', function ($app) {
+            $capsule = new Capsule;
+            $capsule->addConnection([
+                'driver'    => 'mysql',
+                'host'      => '127.0.0.1',
+                'database'  => 'test',
+                'username'  => 'root',
+                'password'  => '19860807',
+                'charset'   => 'utf8',
+                'collation' => 'utf8_unicode_ci',
+                'prefix'    => '',
+            ]);
+    
+            $capsule->setAsGlobal();
+            $capsule->bootEloquent();
+            return $capsule;
+        });
+
+        static::$app->singleton('files', function () {
+            return new Filesystem;
+        });
+
+        static::$app->singleton('cache', function ($app) {
+            $cacheManager = new CacheManager($app);
+            return $cacheManager->driver();
+        });
+    }
+
     /**
      * 清空测试数据库
      *
@@ -20,26 +72,25 @@ class MainTest extends TestCase
     {
         DB::table('categories')->truncate();
         DB::table('category_relationships')->truncate();
-        Category::clearTreeCache();
+        Category::clearTreesCache();
     }
 
     public function testAddCategory()
     {
         $category = Category::createCategory('一级分类', 'App\\Collector');
-        $this->assertDatabaseHas('categories', [
-            'name' => '一级分类',
-            'parent_id' => '0',
-        ]);
+        $count = DB::table('categories')->where('name', '一级分类')->where('parent_id', 0)->count();
+
+        $this->assertEquals(1, $count);
     }
 
     public function testAddChildCategory()
     {
         $category = Category::where('name', '一级分类')->first();
         $newCategory = Category::createCategory('二级分类', 'App\\Collector', $category);
-        $this->assertDatabaseHas('categories', [
-            'name' => '二级分类',
-            'parent_id' => $category->id
-        ]);
+        $this->assertEquals(
+            1,
+            DB::table('categories')->where('name', '二级分类')->where('parent_id', $category->id)->count()
+        );
         $this->assertEquals($newCategory->parent->id, $category->id);
         $this->assertContains($newCategory->id, $category->allChildren->pluck('id'));
     }
@@ -50,7 +101,7 @@ class MainTest extends TestCase
         $level2Category = Category::where('name', '二级分类')->first();
         $level3Category = Category::createCategory('三级分类', 'App\\Collector', $level2Category);
 
-        $this->assertEquals(1, $level2Category->getTree()->children->count());
+        $this->assertEquals(1, count($level2Category->getTree()->children));
         $this->assertContains($level3Category->id, $level1Category->allChildren->pluck('id'));
     }
 
@@ -102,18 +153,26 @@ class MainTest extends TestCase
         $category = Category::where('name', '三级分类5')->first();
         $category_id = $category->id;
         $category->removeCategory();
-        $this->assertDatabaseMissing('categories', [
-            'name' => '三级分类5',
-        ]);
-        $this->assertEquals(0, DB::table('category_relationships')->where('category_id', $category_id)->count());
+        $this->assertEquals(0,
+            DB::table('categories')->where('name', '三级分类5')->count()
+        );
+        $this->assertEquals(0,
+            DB::table('category_relationships')->where('category_id', $category_id)->count()
+        );
     }
 
     public function testCacheTree()
     {
         $category = new Category;
 
-        $this->assertEquals(2, $category->getFullTree()->children->count());
-        $this->assertEquals(2, $category->getTree(Category::where('id', 2)->first())->children->count());
+        $this->assertEquals(1, count($category->getTreeById(1)->children));
+        $this->assertEquals(2, count($category->getTree(Category::where('id', 2)->first())->children));
+    }
+
+    public function testTreesCount()
+    {
+        $category = Category::createRootCategory('Root Category 2');
+        $this->assertEquals(2, count(json_decode(json_encode($category->getTrees()), true)));
     }
 
     public function testCreateNullableRelatedModelCategory()
