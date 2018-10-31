84d50ca7bd07731b40949c1d1c79a58a2f29fdb2
leoding86 <leoding86@msn.com>
2018-10-31 14:53:51 +0800

    add model for caching

---
+++ src/Models/SimplifiedCategory.php
@@ -0,0 +1,77 @@
+<?php namespace LDing\LaravelCategory\Models;
+
+use LDing\LaravelCategory\Models\Category;
+
+class SimplifiedCategory
+{
+    public $id;
+
+    public $name;
+
+    /**
+     * Parent model
+     *
+     * @var SimplifiedCategory
+     */
+    public $parent;
+
+    /**
+     * Children models
+     *
+     * @var array
+     */
+    public $children = [];
+
+    public static function createFromCategory(Category $category)
+    {
+        $simplifiedCategory = new static;
+        $simplifiedCategory->id = $category->id;
+        $simplifiedCategory->name = $category->name;
+        return $simplifiedCategory;
+    }
+
+    public function toJson()
+    {
+        $cat = $this->toArray($this);
+
+        return json_encode($cat);
+    }
+
+    protected function toArray(SimplifiedCategory $category)
+    {
+        $cat = [
+            'id' => $category->id,
+            'parent_id' => $category->parent ? $category->parent->id : null,
+            'name' => $category->name,
+            'children' => [],
+        ];
+
+        foreach ($category->children as $childCategory) {
+            $cat['children'][] = $this->toArray($childCategory);
+        }
+
+        return $cat;
+    }
+
+    /**
+     * Add child category
+     *
+     * @param SimplifiedCategory $child
+     * @return void
+     */
+    public function addChild(SimplifiedCategory $child)
+    {
+        $this->children[] = $child;
+    }
+
+    /**
+     * Set parent category
+     *
+     * @param SimplifiedCategory $parent
+     * @return void
+     */
+    public function setParent(SimplifiedCategory $parent)
+    {
+        $this->parent = $parent;
+    }
+}
